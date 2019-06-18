0f8dd6f6f664a0c93bb531c3bea1691f04adc900
Fawad Ahmed <fawad.ahmed1@snl.int>
2019-06-18 18:47:57 +0500

    Created repo and wrote script for getting started with Kafka

---
+++ producer.py
@@ -0,0 +1,18 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+
+data_source = open('input.txt')
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
+while True:
+    line = data_source.readline()
+    if not line :
+        continue
+    else:
+        print(line)
+        producer.send('numtest', value=line)
+    sleep(2)
